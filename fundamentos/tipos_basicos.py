679d02644bcd53e1a56828f65ea5c47b1cdd1d13
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-04-22 13:43:34 -0400

    alguns exemlos de tipos basicos em python

---
+++ fundamentos/tipos_basicos.py
@@ -0,0 +1,17 @@
+print(True) #tipo boolean
+
+print(False) #tipo boolean
+
+print(1.2 + 1) #float
+
+print('teste de string') #aspas simples, string
+
+print('voce e ' + 3 * 'muito ' + 'legal') #concatenacao com operacao aritimetica
+
+##print(3 + '3') #ambiguidade
+
+print([1, 2, 3]) #lista indexada 
+
+print({'nome': 'Gabriel', 'idade': 25}) #dicionario, chave - valor 
+
+print(None) #equilave a um valor nulo. Null
