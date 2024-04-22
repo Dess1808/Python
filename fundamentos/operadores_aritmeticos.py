b9ac19bbac4905b437ae17c87e8fa8ff725a4991
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-04-22 13:44:54 -0400

    exemplos de operadores aritmeticos em python

---
+++ fundamentos/operadores_aritmeticos.py
@@ -0,0 +1,12 @@
+print(2 + 3) #adicao 
+print(4 - 7) #subtracao
+print(2 * 5.6) #multiplicacao com inteiro e ponto flutuante 
+print(9.4 / 3) #divisao 
+print(9.4 // 3) #divisao com resultado inteiro 
+print(2 ** 8) #exponeciacao
+print(10 % 3) #modulo
+
+a = 12
+b = a
+
+print(a + b)
