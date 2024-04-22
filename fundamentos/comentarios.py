adcc3cc663356d900c633015d12f730a1a79fca8
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-04-22 13:44:20 -0400

    exemplos de comentarios em python - comentarios.py

---
+++ fundamentos/comentarios.py
@@ -0,0 +1,16 @@
+##minhas variavies
+salario = 5667.7
+despesas = 345545.5 
+
+"""
+A ideia e calcular o quanto vai sobrar no fim do mes
+"""
+
+print(salario - despesas) #comentario lateral
+
+'''
+aspas simples tambem funcionam 
+'''
+
+# print('Fim')
+print('Fim de verdade')
