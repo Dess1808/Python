109367cbc81f26ed646bc0d49d7b3fe4c9be5a9b
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-04-22 13:47:21 -0400

    desafio 1, calculo de percentual

---
+++ fundamentos/desafios/percentual.py
@@ -0,0 +1,16 @@
+"""
+calcular o percentual comprometido do salario com os gastos
+"""
+
+#minhas variavies
+salario = 3450.45
+despesas = 2456.2
+
+#regra de 3
+percentual = (despesas * 100) // salario
+
+#percentual = (despesas / salario) * 100
+
+print(salario - despesas)
+print(percentual)
+
