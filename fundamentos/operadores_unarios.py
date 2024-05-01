db7c5fcfdd3ed02d0a774f251f959f4cd3b3ea01
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-05-01 19:55:50 -0400

    exemplos de operadores unarios

---
+++ fundamentos/operadores_unarios.py
@@ -0,0 +1,22 @@
+a = 3 
+
+#a++
+a += 1
+print(a)
+
+#a--
+++a
+print(a)
+
+#transformando em negativo
+print(-a)
+
+#transformando novamente em positivo 
+print(+a)
+
+#exemplos em op not
+print(not 0) 
+print(not 1)
+print(not -2)
+print(not False)
+print(not not True)
