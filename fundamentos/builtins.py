d6c1d9ba3a9ad1f9961d31434d996387740c5e4b
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-05-01 19:54:52 -0400

    exemplos de builtins

---
+++ fundamentos/builtins.py
@@ -0,0 +1,20 @@
+#type()
+type(1)
+__builtins__.type('salve')
+__builtins__.print(10/3)
+
+#__builtins__.help(dir)
+
+#mostra o que esta presente no escopo global
+
+dir()
+
+print(1 + 2)
+print(_ + 1) #funciona ???
+
+dir(__builtins__)
+
+#outros exemplos 
+nome = 'gabriel da silva'
+type(nome)
+__builtins__.len(nome)#retorna a quantidade de caracteres na string
