da5bd30789995fc83d165fa91c8434074e6a2ce5
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-05-02 10:25:58 -0400

    exemplos corretos de builtins e dir

--- fundamentos/builtins.py
+++ fundamentos/builtins.py
@@ -1,20 +1,18 @@
 #type()
-type(1)
-__builtins__.type('salve')
-__builtins__.print(10/3)
+print(__builtins__.type('salve'))
 
 #__builtins__.help(dir)
 
 #mostra o que esta presente no escopo global
+#testar saidas
+#print(dir())
 
-dir()
+#print(dir(__builtins__))
+#print(dir(int))
 
-print(1 + 2)
-print(_ + 1) #funciona ???
+nome = 'Gabriel souza martins'
+print(type(nome))
 
-dir(__builtins__)
-
-#outros exemplos 
-nome = 'gabriel da silva'
-type(nome)
-__builtins__.len(nome)#retorna a quantidade de caracteres na string
+#mesma coisa
+print(__builtins__.len(nome))
+print(len(nome))
