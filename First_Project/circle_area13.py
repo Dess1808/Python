8375aa984b0b5f4a4a9fcb57359a91824ba54970
Gabriel  Martins <gabrielmartins0898@gmail.com>
2024-07-08 10:56:04 -0400

    adicionando uma saida de erro com lib errno

---
+++ First_Project/circle_area13.py
@@ -0,0 +1,30 @@
+##SAINDO COM ERRO
+
+#!/usr/local/bin/python3
+from math import pi
+import sys #argv[]
+import errno
+
+
+#function
+def circle(radius):
+    return pi * radius ** 2
+
+
+def help():
+    print("Eh preciso informar o raio")
+    print(f"formato correto: {sys.argv[0]} <raio>") #show corret input
+
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2: #verification argv lenth
+        help()
+        sys.exit(errno.EPERM)
+    else:
+        radius = float((sys.argv[1])) #converting input
+        area = circle(radius) 
+        print('Area do circulo', area) #without f string, simple exit
+
+    
+
+
